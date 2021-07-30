5afa9f4da24decc1bd5b94b923ad430aabc6c1ad
Nisarg Patel <nrp114@scarletmail.rutgers.edu>
2021-07-29 23:57:05 -0400

    req. changes

--- OpenCV_game_Webapp/settings.py
+++ OpenCV_game_Webapp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+import whitenoise
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
